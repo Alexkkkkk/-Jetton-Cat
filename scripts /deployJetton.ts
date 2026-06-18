f53abf1811153aeda2c79ff71c7ce6e30dbc8ad4
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-06-18 08:41:34 +0300

    Create deployJetton.ts

---
+++ scripts /deployJetton.ts
@@ -0,0 +1,10 @@
+import { beginCell } from "@ton/core";
+
+// Создаем "on-chain" контент (ссылку на метаданные)
+const metadataUrl = "https://raw.githubusercontent.com/Alexkkkkk/Cat/main/metadata.json";
+const content = beginCell()
+    .storeUint(0, 8) // Префикс для URL
+    .storeStringTail(metadataUrl)
+    .endCell();
+
+// Передаем этот 'content' в конструктор JettonMaster при деплое
