c16c0b87e24f180314f4056e654b16d3d11870d1
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-06-18 13:12:57 +0300

    Update deployJetton.ts

--- scripts /deployJetton.ts
+++ scripts /deployJetton.ts
@@ -1,7 +1,7 @@
 import { beginCell } from "@ton/core";
 
 // Создаем "on-chain" контент (ссылку на метаданные)
-const metadataUrl = "https://raw.githubusercontent.com/Alexkkkkk/Cat/main/metadata.json";
+const metadataUrl = "https://raw.githubusercontent.com/Alexkkkkk/-Jetton-Cat/main/metadata.json";
 const content = beginCell()
     .storeUint(0, 8) // Префикс для URL
     .storeStringTail(metadataUrl)
